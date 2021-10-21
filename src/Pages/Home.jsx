2fbe59b64ba786460c0ac841d0b71d72977d1ffc
Zeta <heavenzeta@gmail.com>
2021-10-21 02:34:13 -0300

    Logo not draggable and marginTop fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home () {
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
           <main>
             <div className='flex flex-col items-center justify-center min-h-screen text-white layout gap-2'>
-              <img src={Logo} className='h-1/5' alt='Logo' />
+              <img src={Logo} className='-mt-40' alt='Logo' draggable={false}/>
               <h1 className='text-3xl text-center font-bold text-transparent bg-clip-text bg-gradient-to-b from-red-500 to-red-800'>
                 Starter using Vite, React, Tailwind, Eslint and Router.
                 <p className='text-base'>By EternumZeta</p>
