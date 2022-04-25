eaf7464acee3412d3257860db67ede4df07823bc
EtZeta <heavenzeta@gmail.com>
2022-04-25 09:11:06 -0300

    Minor details

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -12,9 +12,9 @@ export default function Counter () {
   const { count, increase, decrease, reset } = useCounter()
   return (
     <>
-      <div className="py-1 my-2 bg-red-900 text-red-300 rounded border-2 border-red-900">
-      <b className="p-1 text-sm">Counter: {count}</b>
-    </div>
+      <div className='py-1 my-2 bg-red-900 text-red-300 rounded border-2 border-red-900'>
+        <b className='p-1 text-sm'>Counter: {count}</b>
+      </div>
       <div className='flex gap-2'>
         <button
           className='border-2 border-red-800 rounded-lg px-3 py-2 text-red-400 cursor-pointer hover:bg-red-800 hover:text-red-400'
